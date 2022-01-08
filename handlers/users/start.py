16d9d6700d958f94ed0fad501172d72684ac4377
Bexruz Nutfilloyev <63140958+YoshlikMedia@users.noreply.github.com>
2022-01-08 17:35:30 +0500

    add database

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,22 @@
+from datetime import datetime
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+from utils.db_api.mongo import users_db
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Assalomu Alaykum, {message.from_user.full_name}!")
+    req_db = users_db.update_one({'user_id': message.from_user.id}, {'$set': {
+        "user_id": message.from_user.id,
+        "updated": datetime.now(), }
+    }, upsert=True)
+
+    if (req_db.matched_count):
+        textback = "Assalomu Alaykum, <b>{}</b>!".format(message.from_user.full_name)
+    else:
+        textback = "Yana sizni ko'rganimdan xursandman, <b>{}</b>".format(message.from_user.first_name)
+
+    await message.answer(textback)
---
+++ utils/db_api/mongo.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from data.config import IP
+
+client = MongoClient(IP)
+storage = MemoryStorage()
+
+database = client["cartoon-bot"]
+
+users_db = database["users"]
